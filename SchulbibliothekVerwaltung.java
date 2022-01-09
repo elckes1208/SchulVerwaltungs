3f80364c665c6765be6c92df40b9234c0b8ca5ef
elckes1208 <84045490+elckes1208@users.noreply.github.com>
2022-01-09 17:18:05 +0100

    Update SchulbibliothekVerwaltung.java

--- SchulbibliothekVerwaltung.java
+++ SchulbibliothekVerwaltung.java
@@ -27,23 +27,22 @@ public class SchulbibliothekVerwaltung {
         registrierenLeser(5, "WEBER", "Susanne");
         registrierenLeser(18, "MEIER", "Malte");
         registrierenLeser(33, "MEIER", "Ulrike");
-        registrierenLeser(47, "LÖWE", "Robin");
-
+        registrierenLeser(47, "LÖWE", "Paul");
 
         Buch b1 = anlegenBuch("923-123-456", "Grundkurs Java", "Dietmar Abts");
-        Buch b2 = anlegenBuch("923-123-445","Grundkurs JS","Elias Roth");
-        Buch b3 = anlegenBuch("923-123-334","Grundkurs Python","Elias Paul");
+        Buch b2 = anlegenBuch("923-123-445", "Grundkurs JS", "Elias Roth");
+        Buch b3 = anlegenBuch("923-123-334", "Grundkurs Python", "Elias Paul");
         Exemplar exemplar = new Exemplar(b1);
         aufnehmenExemplar(b1);
         aufnehmenExemplar(b2);
         aufnehmenExemplar(b3);
 
-        Ausleihe a1 = ausleihenBuch(exemplar, sucheLeser(0, leser.size(), 12));
+        Ausleihe a1 = ausleihenBuch(exemplar, sucheLeser(12));
         a1.getAusleihDat().setDate(20, 11, 2016);
 
         rueckgebenBuch(a1);
-        Ausleihe a2 = ausleihenBuch(exemplar,registrierenLeser(47, "LÖWE", "Robin"));
-        Ausleihe a3 = ausleihenBuch(exemplar, sucheLeser(0,leser.size(),18));
+        Ausleihe a2 = ausleihenBuch(exemplar, registrierenLeser(47, "LÖWE", "Robin"));
+        Ausleihe a3 = ausleihenBuch(exemplar, sucheLeser(18));
     }
     // Anfang Methoden
 
@@ -52,13 +51,14 @@ public class SchulbibliothekVerwaltung {
      * und f�gt es nach der leserNr aufsteigend sortiert in die Liste leser ein.
      *
      * @param leserNr des Leser-Objekts als Ganzzahl
-     * @param name des Leser-Objekts als String
+     * @param name    des Leser-Objekts als String
      * @param vorname des Leser-Objekts als String
      * @return Bereits existierendes oder neues Leser-Objekt
      */
     public Leser registrierenLeser(int leserNr, String name, String vorname) {
         Leser leserObjekt = new Leser(leserNr, name, vorname);
-        if (leserObjekt == null) leser.add(leserObjekt);
+        if(leserObjekt == null)
+            leser.add(leserObjekt);
         leser.add(leserObjekt);
         return leserObjekt;
     }
@@ -70,17 +70,25 @@ public class SchulbibliothekVerwaltung {
      * @param leserNr des Leser-Objekts als Ganzzahl
      * @return Leser-Objekt oder null, wenn nicht gefunden
      */
-    public Leser sucheLeser(int start, int ende, int leserNr) {
-        ende = leser.size() - 1;
+    public Leser sucheLeser(int leserNr) {
+        Leser gesucht = null;
+        int links = 0;
+        int rechts = leser.size() - 1;
 
-        while (start <= ende){
-            int m = (start+ende)/2;
+        while(links <= rechts && gesucht == null) {
+            int mitte = (rechts + links) / 2;
 
-            if(leserNr == leser.get(m).getLeserNr()) return leser.get(m);
-            else if(leserNr < leser.get(m).getLeserNr()) ende = m - 1;
-            else start = m + 1;
+            if(leser.get(mitte).getLeserNr() == leserNr) {
+                gesucht = leser.get(mitte);
+            } else {
+                if(leserNr < leser.get(mitte).getLeserNr()) {
+                    rechts = mitte - 1;
+                } else {
+                    links = mitte + 1;
+                }
+            }
         }
-        return null;
+        return gesucht;
     }
 
 
@@ -93,10 +101,28 @@ public class SchulbibliothekVerwaltung {
      */
 
     public ArrayList<Leser> generiereSortierteLeserListe() {
-        ArrayList<Leser> neueLeser = new ArrayList<>();
-        for (int i = 0; i < leser.size()-1; i++) {
-        }
+        ArrayList<Leser> neueLeser = new ArrayList<>(this.leser);
+        boolean sortiert = true;
 
+        while(sortiert) {
+            sortiert = false;
+
+            for(int i = 0; i < neueLeser.size() - 1; i++) {
+                //sortiere nach Nachnamen
+                if(neueLeser.get(i).getName().compareTo(neueLeser.get(i + 1).getName()) > 0) {
+                    neueLeser.add(i, neueLeser.remove(i + 1));
+                    sortiert = true;
+                }
+                //sotiere nach gleichem Namen
+                else if(neueLeser.get(i).getName().compareTo(neueLeser.get(i + 1).getName()) == 0) {
+                    //sortiere nach Vornamen
+                    if(neueLeser.get(i).getVorname().compareTo(neueLeser.get(i + 1).getVorname()) > 0) {
+                        neueLeser.add(i, neueLeser.remove(i + 1));
+                        sortiert = true;
+                    }
+                }
+            }
+        }
         return neueLeser;
     }
 
@@ -104,15 +130,15 @@ public class SchulbibliothekVerwaltung {
      * Methode sortiert die Buch-Objekte im Container buecher aufsteigend
      * nach der ISBN-Nummer.
      * Hinweis: Sortieralgorithmus Bubble-Sort
-     *
      */
     public void sortiereBuecher() {
         Buch k;
         boolean getauscht;
+
         do {
             getauscht = false;
-            for (int i = 0; i < buecher.size() - 1; i++) {
-                if (buecher.get(i).getIsbn().compareTo(buecher.get(i + 1).getIsbn()) < 0) {
+            for(int i = 0; i < buecher.size() - 1; i++) {
+                if(buecher.get(i).getIsbn().compareTo(buecher.get(i + 1).getIsbn()) < 0) {
                     continue;
                 }
                 getauscht = true;
@@ -120,21 +146,25 @@ public class SchulbibliothekVerwaltung {
                 buecher.remove(k);
                 buecher.add(i, k);
             }
-        } while (getauscht);
+        } while(getauscht);
     }
 
     /**
      * Methode instanziiert das Buch-Objekt, wenn es noch nicht existiert,
      * und f�gt es in die Liste buecher ein.
      *
-     * @param isbn des Buch-Objekts als String
+     * @param isbn  des Buch-Objekts als String
      * @param titel des Buch-Objekts als String
      * @param autor des Buch-Objekts als String
      * @return Bereits existierendes oder neues Buch-Objekt
      */
     public Buch anlegenBuch(String isbn, String titel, String autor) {
-        Buch buchObjekt = new Buch(isbn, titel, autor);
-        if (buchObjekt == null) buecher.add(buchObjekt);
+        Buch buchObjekt = null;
+
+        if(sucheBuch(isbn) == null){
+            buchObjekt = new Buch(isbn, titel, autor);
+            buecher.add(buchObjekt);
+        }
         return buchObjekt;
     }
 
@@ -145,8 +175,10 @@ public class SchulbibliothekVerwaltung {
      * @return Buch-Objekt oder null, wenn es nicht existiert
      */
     public Buch sucheBuch(String isbn) {
-        for (Buch b : buecher) {
-            if (b.getIsbn().equals(isbn)) return b;
+        for(int i = 0; i < buecher.size() - 1; i++) {
+            if(buecher.get(i).getIsbn().equals(isbn)){
+                return buecher.get(i);
+            }
         }
         return null;
     }
@@ -167,10 +199,10 @@ public class SchulbibliothekVerwaltung {
      * @return Exemplar-Objekt oder null, wenn keins vorhanden
      */
     public Exemplar sucheNichtAusgeliehenesExemplar(Buch buch) {
-        for (int i = 0; i < buecher.size()-1; i++) {
-            if(ausleihen.get(i) == null)
-                System.out.println("Buch wurde nicht ausgeliehen!");
-            return new Exemplar(buch);
+        for(int i = 0; i < buecher.size() - 1; i++) {
+            if(buecher.get(i).getExemplare() == null){
+                return new Exemplar(buch);
+            }
         }
         return null;
     }
@@ -179,12 +211,12 @@ public class SchulbibliothekVerwaltung {
      * Methode erzeugt ein Ausleihe-Objekt, f�gt es dem Ausleihe-Container hinzu
      * und gibt das Ausleihe-Objekt zur�ck.
      *
-     * @param ex ist das Exemplar, das ausgeliehen wird
+     * @param ex     ist das Exemplar, das ausgeliehen wird
      * @param leser, der Leser, der das Buch ausleiht
      * @return neues Ausleihe-Objekt
      */
     public Ausleihe ausleihenBuch(Exemplar ex, Leser leser) {
-        Ausleihe ausleiheObjekt = new Ausleihe(ex,leser);
+        Ausleihe ausleiheObjekt = new Ausleihe(ex, leser);
         ausleihen.add(ausleiheObjekt);
         return ausleiheObjekt;
     }
@@ -205,8 +237,10 @@ public class SchulbibliothekVerwaltung {
         ausleihe.setRueckgabeDat(date);
         ex.setIstAusgeliehen(false);
 
-        if (getMaxAusleihDauer() == 12) return true;
-        else return false;
+        if(getMaxAusleihDauer() == 12)
+            return true;
+        else
+            return false;
     }
 
     /**
@@ -220,8 +254,8 @@ public class SchulbibliothekVerwaltung {
     public ArrayList<Ausleihe> sucheOffeneAusleihen() {
         ArrayList<Ausleihe> ausleiheArrayList = new ArrayList<>();
 
-        for (int i = 0; i < ausleihen.size()-1; i++) {
-            if(getMaxAusleihDauer() == 12)
+        for(int i = 0; i < ausleihen.size() - 1; i++) {
+            if(ausleiheArrayList.get(i).getRueckgabeDat() == null)
                 ausleiheArrayList.add(ausleihen.get(i));
         }
         return ausleiheArrayList;
