2be08f7242be2a11bfe39f6146d02200d6739f35
elckes1208 <84045490+elckes1208@users.noreply.github.com>
2022-01-09 17:18:27 +0100

    Update TestUI.java

--- TestUI.java
+++ TestUI.java
@@ -3,6 +3,25 @@ package SchulBib_Version_Schueler;
 public class TestUI {
     public static void main(String[] args) {
         SchulbibliothekVerwaltung s = new SchulbibliothekVerwaltung();
+
+        System.out.println(s.registrierenLeser(1,"Roth","Elias"));
         System.out.println(s.generiereSortierteLeserListe());
+
+        //Funktioniert nicht richtig
+        System.out.println(s.sucheLeser(1));
+        //Funktioniert nicht s.sortiereBuecher();
+
+        System.out.println(s.anlegenBuch("111-111-111","KA","Jens"));
+        System.out.println(s.sucheBuch("923-123-456"));
+        s.aufnehmenExemplar(s.getBuecher().get(1));
+
+        //Funktioniert nicht richtig
+        System.out.println(s.sucheNichtAusgeliehenesExemplar(s.getBuecher().get(0)));
+
+        System.out.println("HIER BIN ICH");
+        System.out.println(s.ausleihenBuch(s.getBuecher().get(0).getExemplare().get(0),s.getLeser().get(0)));
+
+        //Weiß nicht, ob es richtig geht
+        System.out.println(s.rueckgebenBuch(s.getAusleihen().get(0)));
     }
 }
